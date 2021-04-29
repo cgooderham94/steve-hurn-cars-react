972f51e758f3d8b19d17dc998ace327d3bbd623d
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-04-29 21:53:29 +0100

    Add optima font assets. Update tailwind config to set optima font as base.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      'base': 'Optima, ui-sans-serif, system-ui, sans-serif'
+    }
   },
   variants: {
     extend: {},
