f601e8dd4a728268b63a3e9dcdd02147de3a4912
Charlie Gooderham <charlie@euphoriq.co.uk>
2021-06-17 21:50:44 +0100

    Add base <p> margin. Add basic markup for vehicle intro and specs incl. basic spacing and styling.

--- pages/collection/current-collection/[slug].js
+++ pages/collection/current-collection/[slug].js
@@ -11,16 +11,47 @@ const VehicleDetail = ({ VEHICLE_DATA }) => {
         <StandardLayout>
             <ImgCarousel images={VEHICLE_DATA.vehicleImages} />
 
-            <div className="vehicle__intro">
-                <div className="container mx-auto">
-                    <h1>{ VEHICLE_DATA.title }</h1>
+            <div className="vehicle__intro mt-6 lg:mt-14">
+                <div className="container px-4 mx-auto">
+                    <h1 className="text-2xl lg:text-4xl">{ VEHICLE_DATA.title }</h1>
 
-                    { vehiclePrice }
+                    <div className="vehicle__price mt-5 lg:mt-6 text-green text-lg lg:text-xl">{ vehiclePrice }</div>
+
+                    <div className="vehicle__desc mt-5 lg:mt-6" dangerouslySetInnerHTML={{ __html: VEHICLE_DATA.bodyText }}></div>
                 </div>
             </div>
 
-            { VEHICLE_DATA.year }
-            { VEHICLE_DATA.mileage }
+            <div className="vehicle__specs my-10">
+                <div className="container px-4 mx-auto">
+                    <dl className="flex justify-around">
+                        <div>
+                            <dt>Year:</dt>
+                            <dd>{ VEHICLE_DATA.year }</dd>
+                        </div>
+                        <div>
+                            <dt>Mileage:</dt>
+                            <dd>{ `${VEHICLE_DATA.mileage} miles` }</dd>
+                        </div>
+                        <div>
+                            <dt>Power:</dt>
+                            <dd>{ `${VEHICLE_DATA.power}bhp` }</dd>
+                        </div>
+                        <div>
+                            <dt>0-60:</dt>
+                            <dd>{ `${VEHICLE_DATA.acceleration}s` }</dd>
+                        </div>
+                        <div>
+                            <dt>Torque:</dt>
+                            <dd>{ `${VEHICLE_DATA.torque}nm` }</dd>
+                        </div>
+                        <div>
+                            <dt>Top Speed:</dt>
+                            <dd>{ `${VEHICLE_DATA.topSpeed}mph` }</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+
         </StandardLayout>
     );
 }
@@ -28,18 +59,17 @@ const VehicleDetail = ({ VEHICLE_DATA }) => {
 export default VehicleDetail;
 
 export async function getServerSideProps(context) {
-    console.log(context);
-
     const { data } = await client.query({
         query: gql`
             query GetVehicle($slug: [String]) {
                 entry(section: "vehicles", slug: $slug) {
                     ... on vehicles_vehicles_Entry {
                         id
+                        acceleration
+                        bodyText
                         make
                         mileage
                         model
-                        acceleration
                         power
                         price
                         title
